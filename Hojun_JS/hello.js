ee05a5ab8a99836580f69428037dbeb61cd11a8c
HOJUN2404 <rlaghwns1995@naver.com>
2021-11-05 15:58:46 +0900

    js 연습

--- Hojun_JS/hello.js
+++ Hojun_JS/hello.js
@@ -1,6 +1,36 @@
-var a = 'hello world';
+// var a = 'hello world';
+// let b = 32;
+
+// console.log(a);
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+// b = '호준';
+
+// console.log(typeof b);
+
+// const c = 5;
+
+// console.log(typeof c);
+
+// console.log(b + c);
 
 console.log(a);
+// console.log(b);
+// console.log(c);
+
+var a ;
+let b ;
+const c = 7 ;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+a = 3;
+b = 5;
 
 
-
+console.log(a);
+console.log(b);
+console.log(c);
