c0255d00289ef7bd620d5b54977c76d541b675cf
HOJUN2404 <rlaghwns1995@naver.com>
2021-11-07 17:51:52 +0900

    jest 잼있네

--- Hojun_JS/jsFunction.test.js
+++ Hojun_JS/jsFunction.test.js
@@ -3,8 +3,24 @@ const a = (name) => {
     return name + '입니다';
 }
 
+const race ="100m Dash";
+
+const runners = ["김호준1", "김호준2", "김호준3"];
+
+const result = runners.map((runner, i) => ({name: runner, race, place: i+1}));
+
+console.log(result);
 
 
 test ("단일 파일 jest 테스트 확인", () => {
    expect(a('김호준')).toEqual('김호준입니다');
 });
+
+
+
+const ttt = (a, b) => {
+    return a>b ? a : a;
+};
+
+console.log(ttt(5,2));  
+console.log(ttt(2,5));  
